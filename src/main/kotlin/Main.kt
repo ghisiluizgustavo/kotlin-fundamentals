e60bc6274fe3bac5f4b718aed6076ebbdfaed3e7
Luiz Flores <luiz.flores@avenuecode.com>
2023-06-23 11:56:14 -0300

    initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+    println("Hello World")
+
+    // Mutable
+    var name = "Luiz Gustavo"
+    name = "Another name"
+
+    // Imutable
+    val age = 21
+
+    println(name)
+    println(age)
+}
